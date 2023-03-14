ae7a2771f43d791e6ec97b91c7ef7855f891ef01
isabellarumbewas30 <3103120113@student.smktelkom-pwt.sch.id>
2023-03-14 13:07:43 +0700

    Calculator by Isabella - FINISH

---
+++ calculator.py
@@ -0,0 +1,306 @@
+from PyQt5 import QtCore, QtWidgets
+
+
+class Ui_isabella_calculator_app(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(417, 525)
+        Dialog.setStyleSheet("\n"
+"background-color:qlineargradient(spread:pad, x1:0.466, y1:0, x2:0.556909, y2:0.75, stop:0.272727 rgba(248, 191, 219, 255), stop:0.551136 rgba(232, 200, 226, 255), stop:0.727273 rgba(214, 211, 238, 255), stop:0.9375 rgba(194, 223, 249, 255))")
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setEnabled(True)
+        self.pushButton.setGeometry(QtCore.QRect(30, 350, 75, 71))
+        self.pushButton.setStyleSheet("background-color: #caaed7;\n"
+"font: 18pt \"Eras Demi ITC\";\n"
+"color: white;\n"
+"border-radius: 20px")
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2.setEnabled(True)
+        self.pushButton_2.setGeometry(QtCore.QRect(120, 350, 75, 71))
+        self.pushButton_2.setStyleSheet("background-color: #caaed7;\n"
+"font: 18pt \"Eras Demi ITC\";\n"
+"color: white;\n"
+"border-radius: 20px")
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_3.setEnabled(True)
+        self.pushButton_3.setGeometry(QtCore.QRect(210, 350, 75, 71))
+        self.pushButton_3.setStyleSheet("background-color: #caaed7;\n"
+"font: 18pt \"Eras Demi ITC\";\n"
+"color: white;\n"
+"border-radius: 20px")
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_4.setEnabled(True)
+        self.pushButton_4.setGeometry(QtCore.QRect(30, 270, 75, 71))
+        self.pushButton_4.setStyleSheet("background-color: #caaed7;\n"
+"font: 18pt \"Eras Demi ITC\";\n"
+"color: white;\n"
+"border-radius: 20px")
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_5 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_5.setEnabled(True)
+        self.pushButton_5.setGeometry(QtCore.QRect(120, 270, 75, 71))
+        self.pushButton_5.setStyleSheet("background-color: #caaed7;\n"
+"font: 18pt \"Eras Demi ITC\";\n"
+"color: white;\n"
+"border-radius: 20px")
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.pushButton_6 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_6.setEnabled(True)
+        self.pushButton_6.setGeometry(QtCore.QRect(210, 270, 75, 71))
+        self.pushButton_6.setStyleSheet("background-color:#caaed7;\n"
+"font: 18pt \"Eras Demi ITC\";\n"
+"color: white;\n"
+"border-radius: 20px")
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.pushButton_7 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_7.setEnabled(True)
+        self.pushButton_7.setGeometry(QtCore.QRect(30, 190, 75, 71))
+        self.pushButton_7.setStyleSheet("background-color: #caaed7;\n"
+"font: 18pt \"Eras Demi ITC\";\n"
+"color: white;\n"
+"border-radius: 20px")
+        self.pushButton_7.setObjectName("pushButton_7")
+        self.pushButton_8 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_8.setEnabled(True)
+        self.pushButton_8.setGeometry(QtCore.QRect(120, 190, 75, 71))
+        self.pushButton_8.setStyleSheet("background-color:#caaed7;\n"
+"font: 18pt \"Eras Demi ITC\";\n"
+"color: white;\n"
+"border-radius: 20px")
+        self.pushButton_8.setObjectName("pushButton_8")
+        self.pushButton_9 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_9.setEnabled(True)
+        self.pushButton_9.setGeometry(QtCore.QRect(210, 190, 75, 71))
+        self.pushButton_9.setStyleSheet("background-color:#caaed7;\n"
+"font: 18pt \"Eras Demi ITC\";\n"
+"color: white;\n"
+"border-radius: 20px")
+        self.pushButton_9.setObjectName("pushButton_9")
+        self.pushButton_10 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_10.setEnabled(True)
+        self.pushButton_10.setGeometry(QtCore.QRect(30, 430, 131, 71))
+        self.pushButton_10.setStyleSheet("background-color: #caaed7;\n"
+"font: 18pt \"Eras Demi ITC\";\n"
+"color: white;\n"
+"border-radius: 20px")
+        self.pushButton_10.setObjectName("pushButton_10")
+        self.pushButton_11 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_11.setEnabled(True)
+        self.pushButton_11.setGeometry(QtCore.QRect(300, 430, 75, 71))
+        self.pushButton_11.setStyleSheet("background-color: #caaed7;\n"
+"font: 18pt \"Eras Demi ITC\";\n"
+"color: white;\n"
+"border-radius: 35%")
+        self.pushButton_11.setObjectName("pushButton_11")
+        self.pushButton_12 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_12.setEnabled(True)
+        self.pushButton_12.setGeometry(QtCore.QRect(210, 430, 75, 71))
+        self.pushButton_12.setStyleSheet("background-color:#caaed7;\n"
+"font: 18pt \"Eras Demi ITC\";\n"
+"color: white;\n"
+"border-radius: 20px")
+        self.pushButton_12.setObjectName("pushButton_12")
+        self.pushButton_13 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_13.setEnabled(True)
+        self.pushButton_13.setGeometry(QtCore.QRect(300, 350, 75, 71))
+        self.pushButton_13.setStyleSheet("background-color: #caaed7;\n"
+"font: 18pt \"Eras Demi ITC\";\n"
+"color: white;\n"
+"border-radius: 35%")
+        self.pushButton_13.setObjectName("pushButton_13")
+        self.pushButton_14 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_14.setEnabled(True)
+        self.pushButton_14.setGeometry(QtCore.QRect(300, 270, 75, 71))
+        self.pushButton_14.setStyleSheet("background-color: #caaed7;\n"
+"font: 18pt \"Eras Demi ITC\";\n"
+"color: white;\n"
+"border-radius: 35%")
+        self.pushButton_14.setObjectName("pushButton_14")
+        self.pushButton_15 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_15.setEnabled(True)
+        self.pushButton_15.setGeometry(QtCore.QRect(300, 190, 75, 71))
+        self.pushButton_15.setStyleSheet("background-color: #caaed7;\n"
+"font: 18pt \"Eras Demi ITC\";\n"
+"color: white;\n"
+"border-radius: 35%")
+        self.pushButton_15.setObjectName("pushButton_15")
+        self.pushButton_16 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_16.setEnabled(True)
+        self.pushButton_16.setGeometry(QtCore.QRect(30, 110, 75, 71))
+        self.pushButton_16.setStyleSheet("background-color: #a4d2fa;\n"
+"font: 18pt \"Berlin Sans FB Demi\";\n"
+"color: white;\n"
+"border-radius: 35%")
+        self.pushButton_16.setObjectName("pushButton_16")
+        self.pushButton_17 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_17.setEnabled(True)
+        self.pushButton_17.setGeometry(QtCore.QRect(120, 110, 75, 71))
+        self.pushButton_17.setStyleSheet("background-color: #caaed7;\n"
+"font: 18pt \"Eras Demi ITC\";\n"
+"color: white;\n"
+"border-radius: 35%")
+        self.pushButton_17.setObjectName("pushButton_17")
+        self.pushButton_18 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_18.setEnabled(True)
+        self.pushButton_18.setGeometry(QtCore.QRect(210, 110, 161, 71))
+        self.pushButton_18.setStyleSheet("background-color: #f8a4ca;\n"
+"font: 18pt \"Berlin Sans FB Demi\";\n"
+"color: white;\n"
+"border-radius: 20px")
+        self.pushButton_18.setObjectName("pushButton_18")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(40, 30, 321, 61))
+        self.label.setStyleSheet("background-color: #fef1f7;\n"
+"font: 35pt \"Eras Demi ITC\";\n"
+"color: #c9b4d8")
+        self.label.setObjectName("label")
+
+        self.pushButton.clicked.connect(self.num1)
+        self.pushButton_2.clicked.connect(self.num2)
+        self.pushButton_3.clicked.connect(self.num3)
+        self.pushButton_4.clicked.connect(self.num4)
+        self.pushButton_5.clicked.connect(self.num5)
+        self.pushButton_6.clicked.connect(self.num6)
+        self.pushButton_7.clicked.connect(self.num7)
+        self.pushButton_8.clicked.connect(self.num8)
+        self.pushButton_9.clicked.connect(self.num9)
+        self.pushButton_10.clicked.connect(self.num0)
+        self.pushButton_12.clicked.connect(self.evaluate)
+        self.pushButton_13.clicked.connect(self.add)
+        self.pushButton_14.clicked.connect(self.subtract)
+        self.pushButton_15.clicked.connect(self.multiply)
+        self.pushButton_16.clicked.connect(self.divide)
+        self.pushButton_11.clicked.connect(self.point)
+
+        self.pushButton_17.clicked.connect(self.back)
+        self.pushButton_18.clicked.connect(self.AC)
+
+        self.retranslateUi(isabella_calculator_app)
+        QtCore.QMetaObject.connectSlotsByName(isabella_calculator_app)
+
+    def retranslateUi(self, isabella_calculator_app):
+        _translate = QtCore.QCoreApplication.translate
+        isabella_calculator_app.setWindowTitle(_translate("isabella_calculator_app", "isabella_calculator_app"))
+        self.pushButton.setText(_translate("isabella_calculator_app", "1"))
+        self.pushButton_2.setText(_translate("isabella_calculator_app", "2"))
+        self.pushButton_3.setText(_translate("isabella_calculator_app", "3"))
+        self.pushButton_4.setText(_translate("isabella_calculator_app", "4"))
+        self.pushButton_5.setText(_translate("isabella_calculator_app", "5"))
+        self.pushButton_6.setText(_translate("isabella_calculator_app", "6"))
+        self.pushButton_7.setText(_translate("isabella_calculator_app", "7"))
+        self.pushButton_8.setText(_translate("isabella_calculator_app", "8"))
+        self.pushButton_9.setText(_translate("isabella_calculator_app", "9"))
+        self.pushButton_10.setText(_translate("isabella_calculator_app", "0"))
+        self.pushButton_11.setText(_translate("isabella_calculator_app", "."))
+        self.pushButton_12.setText(_translate("isabella_calculator_app", "="))
+        self.pushButton_13.setText(_translate("isabella_calculator_app", "+"))
+        self.pushButton_14.setText(_translate("isabella_calculator_app", "-"))
+        self.pushButton_15.setText(_translate("isabella_calculator_app", "*"))
+        self.pushButton_16.setText(_translate("isabella_calculator_app", "/"))
+        self.pushButton_17.setText(_translate("isabella_calculator_app", "Back"))
+        self.pushButton_18.setText(_translate("isabella_calculator_app", "AC"))
+        self.label.setText(_translate("isabella_calculator_app", ""))
+
+    def point(self):
+
+        number = self.label.text()
+        self.label.setText(number + ".")
+
+    def num1(self):                         # Fungsi num1 merupakan sebuah method yang berfungsi untuk menambahkan angka 1 pada label teks           
+                                            # agar ketika tombol angka 1 pada kalkulator dapat ditekan
+        number = self.label.text()          # Method akan menyimpan ke dalam variabel number.  
+        self.label.setText(number + "1")    # Kemudian pada baris kedua, method ini menambahkan string "1" 
+                                            # pada variabel number menggunakan operator "+" dan mengupdate nilai label dengan hasil penambahan tersebut menggunakan method setText()
+
+    def num2(self):
+        number = self.label.text()
+        self.label.setText(number + "2")
+
+    def num3(self):
+        number = self.label.text()
+        self.label.setText(number + "3")
+
+    def num4(self):
+        number = self.label.text()
+        self.label.setText(number + "4")
+
+    def num5(self):
+        number = self.label.text()
+        self.label.setText(number + "5")
+
+    def num6(self):
+        number = self.label.text()
+        self.label.setText(number + "6")
+
+    def num7(self):
+        number = self.label.text()
+        self.label.setText(number + "7")
+
+    def num8(self):
+        number = self.label.text()
+        self.label.setText(number + "8")
+
+    def num9(self):
+        number = self.label.text()
+        self.label.setText(number + "9")
+
+    def num0(self):
+        number = self.label.text()
+        self.label.setText(number + "0")
+
+    def evaluate(self):
+
+        input_text  = self.label.text()
+
+        try:
+                # Evaluasi input
+                result = eval(input_text) #Fungsi ini digunakan untuk mengevaluasi input, input yang dievaluasi adalah input_text.
+                result = round(result, 10) # Fungsi ini digunakan untuk membulatkan hasil evaluasi hingga 10 digit desimal.
+                result = str(result) #Fungsi ini digunakan untuk mengubah hasil evaluasi yang sudah dikonversi menjadi tipe data Decimal menjadi tipe data string
+                # Menampilkan hasil evaluasi
+                self.label.setText(result)
+
+        except Exception as e:
+            # Jika input tidak valid, menampilkan pesan kesalahan
+                self.label.setText("Error")  
+
+    def add(self):
+
+        number = self.label.text()
+        self.label.setText(number + "+")
+
+    def subtract(self):
+        number = self.label.text()
+        self.label.setText(number + "-")
+
+    def multiply(self):
+        number = self.label.text()
+        self.label.setText(number + "*")
+
+    def divide(self):
+        number = self.label.text()
+        self.label.setText(number + "/")
+
+    def back(self):
+
+        number = self.label.text()
+        number = number[1:]
+        self.label.setText(number)
+
+    def AC(self):
+
+        self.label.setText("")
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    isabella_calculator_app = QtWidgets.QWidget()
+    ui = Ui_isabella_calculator_app()
+    ui.setupUi(isabella_calculator_app)
+    isabella_calculator_app.show()
+    sys.exit(app.exec_())
